d21417c8c0ee1ae96d724c30713f31ca9e29f0e4
cristiansegunda1-max <cristiansegunda1@gmail.com>
2026-02-22 18:22:51 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let audio = document.getElementById("audio");
+const cover = document.querySelector(".cover");
+const background = document.querySelector(".background");
+const progress = document.querySelector(".progress");
 
 function togglePlay() {
   if (audio.paused) {
@@ -7,3 +10,17 @@ function togglePlay() {
     audio.pause();
   }
 }
+
+/* Fondo dinámico */
+cover.onload = () => {
+  background.style.backgroundImage = `url(${cover.src})`;
+  background.style.backgroundSize = "cover";
+  background.style.backgroundPosition = "center";
+  background.style.filter = "blur(80px) brightness(0.4)";
+};
+
+/* Barra de progreso */
+audio.addEventListener("timeupdate", () => {
+  const percent = (audio.currentTime / audio.duration) * 100;
+  progress.style.width = percent + "%";
+});
